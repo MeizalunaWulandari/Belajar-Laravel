7d5e7c7d67995b82ab047103956ba4809d5657a5
Meizaluna Wulandari <axelmaxi2810@gmail.com>
2022-11-13 10:45:55 +0800

    Belajar PHP Materi 3

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/style.css">
+	<title>WPU Blog | About</title>
+</head>
+<body>
+	<h1>Halaman About</h1>
+	<h3><?= $name ?></h3>
+	<p><?= $email ?></p>
+	<img src="img/<?= $image ?>" alt="Meizaluna Wulandari" width="200">
+</body>
+</html>
