c10a1352a01b3fb461f069652fd9d55abec0c1d3
Meizaluna Wulandari <axelmaxi2810@gmail.com>
2022-11-14 09:02:26 +0800

    Belajar Laravel Materi 5 - Model, Collection & Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,54 +31,6 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-    [
-        'title' => 'Judul Post Pertama',
-        'author' => 'Meizaluna Wulandari',
-        'slug' => 'Judul-Post-Pertama',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus debitis voluptatum possimus, a, dolorum vero tempora consequuntur, non expedita nulla, officia eos. Iusto necessitatibus eos porro ut qui ipsum perferendis odit nam natus voluptates incidunt cumque pariatur exercitationem ullam quae placeat perspiciatis sunt sapiente voluptatum consequuntur, rerum recusandae. Ipsa, illum vel? Illo cupiditate doloremque corrupti aut neque quidem quas dicta expedita obcaecati maxime exercitationem nam tempore, et adipisci autem modi dolor ab corporis excepturi rem, dolore a voluptas! Quibusdam, odio?'
-    ],
-    [
-        'title' => 'Judul Post Kedua',
-        'author' => 'Joko Widodo',
-        'slug' => 'Judul-Post-Kedua',
-        'body' => '
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde odit assumenda odio architecto totam vitae facilis ut aspernatur a fugit necessitatibus eaque omnis rem earum numquam molestiae, iste inventore, expedita, perspiciatis dolorem maiores corrupti cum possimus velit, quia! Consequuntur optio dignissimos sit error praesentium, voluptatibus libero modi exercitationem delectus nam corporis inventore, quasi eos at mollitia, molestias culpa, rem fuga architecto. Id, natus nobis impedit exercitationem blanditiis nam!'
-    ],
-];
+Route::get('/blog', [PostController::class, 'index']);
 
-    return view('posts', [
-        'title' => 'Blog',
-        'posts' => $blog_posts
-    ]);
-});
-
-Route::get('posts/{slug}', function($slug){
-    $blog_posts = [
-    [
-        'title' => 'Judul Post Pertama',
-        'author' => 'Meizaluna Wulandari',
-        'slug' => 'Judul-Post-Pertama',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus debitis voluptatum possimus, a, dolorum vero tempora consequuntur, non expedita nulla, officia eos. Iusto necessitatibus eos porro ut qui ipsum perferendis odit nam natus voluptates incidunt cumque pariatur exercitationem ullam quae placeat perspiciatis sunt sapiente voluptatum consequuntur, rerum recusandae. Ipsa, illum vel? Illo cupiditate doloremque corrupti aut neque quidem quas dicta expedita obcaecati maxime exercitationem nam tempore, et adipisci autem modi dolor ab corporis excepturi rem, dolore a voluptas! Quibusdam, odio?'
-    ],
-    [
-        'title' => 'Judul Post Kedua',
-        'author' => 'Joko Widodo',
-        'slug' => 'Judul-Post-Kedua',
-        'body' => '
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde odit assumenda odio architecto totam vitae facilis ut aspernatur a fugit necessitatibus eaque omnis rem earum numquam molestiae, iste inventore, expedita, perspiciatis dolorem maiores corrupti cum possimus velit, quia! Consequuntur optio dignissimos sit error praesentium, voluptatibus libero modi exercitationem delectus nam corporis inventore, quasi eos at mollitia, molestias culpa, rem fuga architecto. Id, natus nobis impedit exercitationem blanditiis nam!'
-    ],
-];
-
-$new_post= [];
-foreach ($blog_posts as $post) {
-    if ($post['slug'] === $slug) {
-        $new_post  =  $post;
-    }
-}
-    return view('post',[
-        'title' => 'Single Post',
-        'post' => $new_post
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
