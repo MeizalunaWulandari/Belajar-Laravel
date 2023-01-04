7ffa3fab33bdec37b18c3d84cc945e5004a48e4e
Meizaluna Wulandari <axelmaxi2810@gmail.com>
2023-01-04 12:26:06 +0800

    Belajar Laravel - Materi 9 Database Seeder

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.main')
 @section('container')
 	@foreach($posts as $post)
-		<article class="mb-5">
+		<article class="mb-5 border-bottom">
 			<h1>
 				<a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
 			</h1>
+			<p>By. <a href="/categories/{{ $post->user->slug }}">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 			{{-- <h5>By: {{ $post['author'] }}</h5> --}}
 			<p>{{ $post->excerpt }}</p>
+			<a href="/posts/{{ $post->slug }}">Read more..</a>
 		</article>
 	@endforeach
 @endsection
